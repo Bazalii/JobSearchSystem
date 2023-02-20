4c964c91601a9db1c04a59d1fde0026a1daf99e4
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-02-20 14:20:19 +0300

    feat: realise UserController

---
+++ web/src/main/kotlin/users/controllers/UserController.kt
@@ -0,0 +1,45 @@
+package users.controllers
+
+import users.models.*
+import users.services.IUserService
+import java.util.*
+import javax.enterprise.context.RequestScoped
+import javax.ws.rs.*
+
+@RequestScoped
+@Path("/user")
+class UserController(
+    private var _userService: IUserService,
+) {
+
+    @POST
+    fun add(userCreationRequest: UserCreationRequest) {
+        _userService.add(
+            UserCreationModel(
+                login = userCreationRequest.login,
+                password = userCreationRequest.password
+            )
+        )
+    }
+
+    @GET
+    fun getById(id: UUID): UserResponse {
+        val user = _userService.getById(id)
+
+        return UserResponse(
+            id = user.id,
+            login = user.login,
+            password = user.password
+        )
+    }
+
+    @PUT
+    fun updatePassword(updatePasswordRequest: UpdatePasswordRequest) {
+        _userService.updatePassword(updatePasswordRequest.id, updatePasswordRequest.password)
+    }
+
+    @DELETE
+    fun removeById(id: UUID) {
+        _userService.removeById(id)
+    }
+}
