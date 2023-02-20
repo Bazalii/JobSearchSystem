7ed345edb6a50e182e19e0d2cc092bf67eddafe1
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-02-20 14:06:45 +0300

    feat: realise PanacheUserRepository

---
+++ data/src/main/kotlin/users/repositories/PanacheUserRepository.kt
@@ -0,0 +1,32 @@
+package users.repositories
+
+import exceptions.EntityNotFoundException
+import io.quarkus.hibernate.orm.panache.kotlin.PanacheRepositoryBase
+import users.models.UserDbModel
+import java.util.*
+import javax.enterprise.context.ApplicationScoped
+
+@ApplicationScoped
+class PanacheUserRepository : PanacheRepositoryBase<UserDbModel, UUID> {
+
+    fun add(user: UserDbModel) {
+        persist(user)
+    }
+
+    fun getById(id: UUID): UserDbModel {
+        return findById(id) ?: throw EntityNotFoundException("Entity not found!")
+    }
+
+    fun removeById(id: UUID) {
+        val user = getById(id)
+
+        deleteById(id)
+    }
+
+    fun updatePassword(userId: UUID, password: String) {
+        val userDbModel =
+            findById(userId) ?: throw EntityNotFoundException("Entity not found!")
+
+        userDbModel.password = password
+    }
+}
