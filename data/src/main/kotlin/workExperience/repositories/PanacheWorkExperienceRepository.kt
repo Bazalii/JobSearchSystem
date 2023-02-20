b6625d4b43bcb7ae9c7a02dea4739cca788c4671
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-02-20 14:08:38 +0300

    feat: realise PanacheWorkExperienceRepository

---
+++ data/src/main/kotlin/workExperience/repositories/PanacheWorkExperienceRepository.kt
@@ -0,0 +1,37 @@
+package workExperience.repositories
+
+import exceptions.EntityNotFoundException
+import io.quarkus.hibernate.orm.panache.kotlin.PanacheRepositoryBase
+import workExperience.models.WorkExperienceItemDbModel
+import java.util.*
+import javax.enterprise.context.ApplicationScoped
+
+@ApplicationScoped
+class PanacheWorkExperienceRepository : PanacheRepositoryBase<WorkExperienceItemDbModel, UUID> {
+
+    fun add(workExperienceItemDbModel: WorkExperienceItemDbModel) {
+        persist(workExperienceItemDbModel)
+    }
+
+    fun getById(id: UUID): WorkExperienceItemDbModel {
+        return findById(id) ?: throw EntityNotFoundException("Entity not found!")
+    }
+
+    fun getAllByUserId(id: UUID): List<WorkExperienceItemDbModel> {
+        return list("user_id", id)
+    }
+
+    fun update(workExperienceItemDbModel: WorkExperienceItemDbModel) {
+        val dbModel = findById(workExperienceItemDbModel.id) ?: throw EntityNotFoundException("Entity not found!")
+
+        dbModel.place = workExperienceItemDbModel.place
+        dbModel.position = workExperienceItemDbModel.position
+        dbModel.dates = workExperienceItemDbModel.dates
+    }
+
+    fun removeById(id: UUID) {
+        val dbModel = getById(id)
+
+        deleteById(id)
+    }
+}
