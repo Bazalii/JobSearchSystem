acdf1e1779139c0976e26c9c4b9ac635ea6ef23b
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-02-20 14:18:49 +0300

    feat: realise DTOs for resumes

---
+++ web/src/main/kotlin/resumes/models/ResumeCreationRequest.kt
@@ -0,0 +1,15 @@
+package resumes.models
+
+import java.util.*
+
+data class ResumeCreationRequest(
+    var name: String,
+    var currentJob: String,
+    var quote: String,
+    var languages: String,
+    var frameworks: String,
+    var databases: String,
+    var otherTechnologies: String,
+    var additionalInformation: String,
+    var userId: UUID,
+)
