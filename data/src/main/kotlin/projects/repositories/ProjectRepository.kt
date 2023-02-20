3c88ea2d494a654d48d233f4dfaea0363a07621e
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-02-20 14:13:29 +0300

    feat: realise ProjectRepository

---
+++ data/src/main/kotlin/projects/repositories/ProjectRepository.kt
@@ -0,0 +1,55 @@
+package projects.repositories
+
+import projects.extensions.toProject
+import projects.models.Project
+import projects.models.ProjectDbModel
+import users.repositories.PanacheUserRepository
+import java.util.*
+import javax.enterprise.context.ApplicationScoped
+
+@ApplicationScoped
+class ProjectRepository(
+    private var _panacheUserRepository: PanacheUserRepository,
+    private var _panacheProjectRepository: PanacheProjectRepository
+) : IProjectRepository {
+
+    override fun add(project: Project) {
+        _panacheProjectRepository.add(
+            ProjectDbModel(
+                id = project.id,
+                name = project.name,
+                link = project.link,
+                year = project.year,
+                user = _panacheUserRepository.getById(project.userId)
+            )
+        )
+    }
+
+    override fun getById(id: UUID): Project {
+        val dbModel = _panacheProjectRepository.getById(id)
+
+        return dbModel.toProject()
+    }
+
+    override fun getAllByUserId(id: UUID): List<Project> {
+        val dbModels = _panacheProjectRepository.getAllByUserId(id)
+
+        return dbModels.map { it.toProject() }
+    }
+
+    override fun update(project: Project) {
+        _panacheProjectRepository.update(
+            ProjectDbModel(
+                id = project.id,
+                name = project.name,
+                link = project.link,
+                year = project.year,
+                user = _panacheUserRepository.getById(project.userId)
+            )
+        )
+    }
+
+    override fun removeById(id: UUID) {
+        _panacheProjectRepository.removeById(id)
+    }
+}
