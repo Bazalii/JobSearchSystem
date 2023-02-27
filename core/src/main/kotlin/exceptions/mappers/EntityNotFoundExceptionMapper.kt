b0858aeda96ab4c7d5f94fdd9825bd322f5922b4
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-02-27 22:13:33 +0300

    feat: realise EntityNotFoundExceptionMapper

---
+++ core/src/main/kotlin/exceptions/mappers/EntityNotFoundExceptionMapper.kt
@@ -0,0 +1,14 @@
+package exceptions.mappers
+
+import exceptions.EntityNotFoundException
+import javax.ws.rs.core.Response
+import javax.ws.rs.ext.ExceptionMapper
+import javax.ws.rs.ext.Provider
+
+@Provider
+class EntityNotFoundExceptionMapper : ExceptionMapper<EntityNotFoundException> {
+
+    override fun toResponse(exception: EntityNotFoundException?): Response {
+        return Response.status(404).entity(exception?.message).build()
+    }
+}
