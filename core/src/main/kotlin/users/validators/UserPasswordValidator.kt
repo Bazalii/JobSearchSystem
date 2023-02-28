4c358691bdf77c02ef3f8b1df17bbeafd49af341
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-02-28 23:35:20 +0300

    feat: realise UserPasswordValidator

--- core/src/main/kotlin/users/validators/UserPasswordValidator.kt
+++ core/src/main/kotlin/users/validators/UserPasswordValidator.kt
@@ -8,10 +8,14 @@ import javax.enterprise.context.ApplicationScoped
 class UserPasswordValidator : IThrowingValidator<String> {
 
     override fun validate(checkedObject: String) {
-        if (!checkedObject.matches("[a-z]*".toRegex()) ||
-            !checkedObject.matches("[A-Z]*".toRegex()) ||
-            !checkedObject.matches("[@#\$]*".toRegex()) ||
-            !checkedObject.matches("[1-9]*".toRegex())
+        if (checkedObject.length < 8 || checkedObject.length > 30) {
+            throw InvalidEntityException("Incorrect password length! It should be less than 30 characters but more than 8.")
+        }
+
+        if (!checkedObject.contains("[a-z]".toRegex()) ||
+            !checkedObject.contains("[A-Z]".toRegex()) ||
+            !checkedObject.contains("[@#\$]".toRegex()) ||
+            !checkedObject.contains("[1-9]".toRegex())
         ) {
             throw InvalidEntityException(
                 "Password should contain at least one capital English letter, one lowercase" +
