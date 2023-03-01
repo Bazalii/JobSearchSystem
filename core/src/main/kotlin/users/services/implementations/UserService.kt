c276389fa4966c731d8fb642bb17b25701ebf33f
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-03-01 18:17:08 +0300

    feat: add usage of UserValidator and UserPasswordValidator

--- core/src/main/kotlin/users/services/implementations/UserService.kt
+++ core/src/main/kotlin/users/services/implementations/UserService.kt
@@ -1,18 +1,24 @@
 package users.services.implementations
 
+import commonClasses.IThrowingValidator
 import users.models.User
 import users.models.UserCreationModel
 import users.repositories.IUserRepository
 import users.services.IUserService
+import users.validators.UserPasswordValidator
 import java.util.*
 import javax.enterprise.context.ApplicationScoped
 
 @ApplicationScoped
 class UserService(
     private var _userRepository: IUserRepository,
+    private var _userValidator: IThrowingValidator<UserCreationModel>,
+    private val _userPasswordValidator: UserPasswordValidator,
 ) : IUserService {
 
     override fun add(userCreationModel: UserCreationModel) {
+        _userValidator.validate(userCreationModel)
+
         _userRepository.add(
             User(
                 id = UUID.randomUUID(),
@@ -27,6 +33,8 @@ class UserService(
     }
 
     override fun updatePassword(userId: UUID, password: String) {
+        _userPasswordValidator.validate(password)
+
         _userRepository.updatePassword(userId, password)
     }
 
