d775f761f076c38c932a58759808d425a85f8b9e
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-03-02 21:31:21 +0300

    fix: remove password from UserResponse

--- web/src/main/kotlin/users/models/UserResponse.kt
+++ web/src/main/kotlin/users/models/UserResponse.kt
@@ -5,5 +5,4 @@ import java.util.*
 data class UserResponse(
     var id: UUID = UUID.fromString("00000000-0000-0000-0000-000000000000"),
     var login: String = "",
-    var password: String = "",
 )
