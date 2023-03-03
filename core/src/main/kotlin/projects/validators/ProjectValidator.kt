96046b22de76e9673dd572c49d9f9f90bc1dbfe5
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-03-03 13:38:25 +0300

    feat: realise ProjectValidator

---
+++ core/src/main/kotlin/projects/validators/ProjectValidator.kt
@@ -0,0 +1,28 @@
+package projects.validators
+
+import commonClasses.IThrowingValidator
+import exceptions.InvalidEntityException
+import projects.models.Project
+import javax.enterprise.context.ApplicationScoped
+
+@ApplicationScoped
+class ProjectValidator : IThrowingValidator<Project> {
+
+    override fun validate(checkedObject: Project) {
+        if (!checkedObject.name.matches("[a-zA-Z]*".toRegex())) {
+            throw InvalidEntityException("Project name should consist only of English letters!")
+        }
+
+        if (checkedObject.name.length < 5 || checkedObject.name.length > 30) {
+            throw InvalidEntityException("Incorrect project name length! It should be less than 30 characters but more than 5.")
+        }
+
+        if (checkedObject.link.length < 4 || checkedObject.link.length > 200) {
+            throw InvalidEntityException("Incorrect project link length! It should be less than 200 characters but more than 4.")
+        }
+
+        if (checkedObject.year < 1843) {
+            throw InvalidEntityException("Programming did not exist back to that days.")
+        }
+    }
+}
