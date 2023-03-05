cd4ea5e6be4a0a315302cac60def900ca800799d
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-03-05 16:00:03 +0300

    feat: realise FrameworkService

---
+++ core/src/main/kotlin/frameworks/services/IFrameworkService.kt
@@ -0,0 +1,11 @@
+package frameworks.services
+
+import frameworks.models.Framework
+import frameworks.models.FrameworkCreationModel
+import java.util.*
+
+interface IFrameworkService {
+    fun add(frameworkCreationModel: FrameworkCreationModel): Framework
+    fun getAll(): List<Framework>
+    fun removeById(id: UUID): Framework
+}
