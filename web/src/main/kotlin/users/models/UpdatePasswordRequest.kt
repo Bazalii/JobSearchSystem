1d6fc172dc67d8a00416943b977f0c37fda6dd48
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-03-20 12:44:23 +0300

    fix: remove userId from ResumeCreationRequest and UpdatePasswordRequest

--- web/src/main/kotlin/users/models/UpdatePasswordRequest.kt
+++ web/src/main/kotlin/users/models/UpdatePasswordRequest.kt
@@ -1,8 +1,5 @@
 package users.models
 
-import java.util.*
-
 data class UpdatePasswordRequest(
-    var id: UUID = UUID.fromString("00000000-0000-0000-0000-000000000000"),
     var password: String = "",
 )
