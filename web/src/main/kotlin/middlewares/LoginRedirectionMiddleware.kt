7c2ddd6eea52fb6bf7bca6392cc4a23cd3e41624
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-03-31 18:34:46 +0300

    feat: realise LoginRedirectionMiddleware

---
+++ web/src/main/kotlin/middlewares/LoginRedirectionMiddleware.kt
@@ -0,0 +1,28 @@
+package middlewares
+
+import java.net.URI
+import javax.annotation.Priority
+import javax.ws.rs.container.ContainerRequestContext
+import javax.ws.rs.container.ContainerRequestFilter
+import javax.ws.rs.core.Response
+import javax.ws.rs.ext.Provider
+
+@Provider
+@Priority(1000)
+class LoginRedirectionMiddleware : ContainerRequestFilter {
+
+    override fun filter(requestContext: ContainerRequestContext) {
+        val requestUri = requestContext.uriInfo.requestUri.toString()
+        val wantedPage = requestUri.substringAfter("pages/")
+
+        if (requestUri.contains("pages") &&
+            wantedPage != "login" &&
+            wantedPage != "registration" &&
+            !requestContext.headers.containsKey("Authorization")
+        ) {
+            requestContext.abortWith(
+                Response.seeOther(URI.create("http://localhost:8080/pages/login#${wantedPage}")).build()
+            )
+        }
+    }
+}
