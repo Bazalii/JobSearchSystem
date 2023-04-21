a385c6772b375709fe96b9962d0ef4221b462272
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-04-21 20:18:01 +0300

    feat: realise UserRoleValidator

---
+++ core/src/main/kotlin/users/validators/UserRoleValidator.kt
@@ -0,0 +1,17 @@
+package users.validators
+
+import commonClasses.IThrowingValidator
+import exceptions.InvalidEntityException
+import javax.enterprise.context.ApplicationScoped
+
+@ApplicationScoped
+class UserRoleValidator : IThrowingValidator<String> {
+
+    override fun validate(checkedObject: String) {
+        if (checkedObject != "User" &&
+            checkedObject != "HR" &&
+            checkedObject != "Admin") {
+            throw InvalidEntityException("Invalid role!")
+        }
+    }
+}
